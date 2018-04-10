b7d530799ca3974e6f9c97385aca1ad51e2da03f
mmiranda96 <mikemp96@gmail.com>
2018-04-09 19:30:38 -0500

    Practice 8

--- practices/8/input.c
+++ practices/8/input.c
@@ -1,23 +1,59 @@
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
-
-#define printDebug(...) do {                            \
+#define printDebug(...)                                 \
+do                                                      \
+{                                                       \
     char *isDebug;                                      \
     isDebug = getenv("DEBUG");                          \
-    if (isDebug != NULL && !strcasecmp(isDebug, "y")) {  \
+    if (isDebug != NULL && !strcasecmp(isDebug, "y"))   \
+    {                                                   \
         fprintf(stderr, __VA_ARGS__);                   \
     }                                                   \
-}while(0)
+} while (0)
 
 int main()
 {
+    char *fileName = "input.txt";
+    FILE *input;
+    char *search = "mundo";
+    char *replace = "MUNDOMUNDO";
+    char word[255];
+    long searchPos = 0;
     int character;
-    while(!feof(stdin)) {
-        character = fgetc(stdin);
-        fputc(character, stdout);
-        printDebug("Error character %c\n", character);
-    }
+    input = fopen(fileName, "r");
+    
+    while (1) {
+        fscanf(input,"%s",word);
+        if(feof(input)){
+            break;
+        }
 
+        if(strcmp(word,search) == 0) {
+            printDebug("Found %s\n", word);
+            fseek(input,-1*strlen(search),SEEK_CUR);
+            searchPos = ftell(input);
+            printDebug("searchPos = %ld\n", searchPos);
+            rewind(input);
+
+            while(ftell(input) != searchPos) {
+                character = fgetc(input);
+                fputc(character,stdout);
+            }
+        
+            fputs(replace,stdout);
+            fseek(input,strlen(word),SEEK_CUR);
+            while(1){
+                character = fgetc(input);
+                if(feof(input)) {
+                    break;
+                }
+                fputc(character,stdout);
+            }
+            
+            break;
+        }
+    }
+    
     return 0;
 }
